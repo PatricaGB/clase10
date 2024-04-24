8d611495bfe6d47f80b6928a66cb48fb4bfc8bd1
Patricia Guzman <90807349+PatricaGB@users.noreply.github.com>
2024-04-24 15:28:11 -0500

    segunda clase sobre ejemplos de componentes en react para proyecto web

--- primera-actividad/src/App.js
+++ primera-actividad/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import Saludo from './practica/saludo';
-import Saludo2 from './practica/saludo2';
-import Producto from './practica/producto';
+import Contador from './practica/contador';
+import Contclick from './practica/Contclick';
+import Titulo from './practica/Titulo';
+import Contador2 from './practica/Contador_2';
 
 
-const elemento =<h1>Hola Mundo</h1>
-const name="Pat"
-const elemento2=<h2>Hola {name}</h2>
 
 function app() {
     return(
         <div className="app">
-            {elemento}
-            {elemento2}
-            <Saludo/>
-            <Saludo2/>
-            <Producto nombre="Mesa" descripcion="elemento de oficina" precio={500}/>
-            <Producto nombre="Vestido" imagen="/logo192.png" alternativo="imagen de vestido" descripcion="prenda para mujer" precio={25000}/>
+            <Contador inicial="5" incremento="3" decremento="3"/>
+            <Contador inicial="2" incremento="12" decremento="4"/>
+            <Contador/>
+            <Contclick/>
+            <Titulo/>
+            <Contador2/>
         </div>
     );
 }
